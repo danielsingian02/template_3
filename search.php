ff38e92fd035d17909a5852fb03abadbb2c7955e
Daniel Singian <daniel.singian02@gmail.com>
2022-10-18 15:56:09 +0800

    Added styling to the search page

--- search.php
+++ search.php
@@ -1,60 +1,58 @@
 <?php
-
 get_header(); ?>
-  <div class="site-content-search">
-  <div class="search_form">
-		<form method="get" id="searchform" action="<?php bloginfo('url'); ?>">
-			<div>
-			<input class="text-buttonsearch" type="text" value=" " name="s" id="buttonsearch" placeholder="Search..."/>
-			<input type="submit" class="submit buttonsearch" name="submit" value="<?php _e('Search');?>" />
-			</div>
-			</form>
-		</div>
-	  <header class="page-header-search">
-		  <h1 class="page-title-search">
-			  <?php
-				  /* translators: %s: the search query */
-				  printf( esc_html__( 'Search Results for: %s', 'scaffold' ), '<span>' . get_search_query() . '</span>' );
-			  ?>
-		  </h1>
-	  </header><!-- .page-header -->
-	    <?php
-	    if ( have_posts() ) :
 
-	      while ( have_posts() ) :
+<div class="site-content-search">
+	<div class="search-results">
+		<header class="page-header-search">
+			<h1 class="page-title-search">
+				<?php
+					/* translators: %s: the search query */
+					printf( esc_html__( 'Search Results for: %s', 'scaffold' ), '<span>' . get_search_query() . '</span>' );
+				?>
+			</h1>
+		</header><!-- .page-header -->
+		<?php
+		if ( have_posts() ) :
 
-	        the_post();
-	        ?>
+			while ( have_posts() ) :
 
-	        <article <?php post_class(); ?>>
+				the_post();
+				?>
 
-			  <?php the_post_thumbnail( 'my-custom-image-size' ); ?>
+				<article <?php post_class(); ?>>
+					<a href="<?php echo esc_url( get_permalink() ); ?>" target="_blank" >
 
-	          <header class="entry-header-search">
-	            <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
-	          </header><!-- .entry-header -->
+						<div class="content">
+							<?php the_post_thumbnail( 'my-custom-image-size' ); ?>
 
-	          <div class="entry-content-search">
-	            <?php the_content( esc_html__( 'Continue reading &rarr;', 'my-custom-theme' ) ); ?>
-	          </div><!-- .entry-content -->
+							<header class="entry-header-search">
+								<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+							</header><!-- .entry-header -->
 
-	        </article><!-- #post-## -->
+							<div class="entry-content-search">
+								<?php the_content( esc_html__( 'Continue reading &rarr;', 'my-custom-theme' ) ); ?>
+							</div><!-- .entry-content -->
+						</div>
+					</a>
+				</article><!-- #post-## -->
 
-	        <?php
-	        // If comments are open or we have at least one comment, load up the comment template.
-	        if ( comments_open() || get_comments_number() ) :
-	          comments_template();
-	        endif;
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-	      endwhile;
+			endwhile;
 
-		  the_posts_navigation();
+			the_posts_navigation();
 
-	    else :
-	      get_template_part( 'content-none' );
-	    endif;
+			else :
+			get_template_part( 'content-none' );
+		endif;
 		?>
-		</div><!-- .site-content -->
-        <?php
+	</div>
+</div><!-- .site-content -->
+
+<?php
 get_footer();
 ?>
