2f40902d90e316812fc333416ecb1f1684870ab1
Daniel Singian <daniel.singian02@gmail.com>
2022-11-14 16:38:55 +0800

    Updated the html structure

--- header.php
+++ header.php
@@ -10,65 +10,69 @@
 	wp_head();
 	?>
 </head>
-<header class="template_3_head">
-<!-- navigation menu -->
 
-	<nav class="navbar">
-		<div class="navbar-container">
-			<div class="logo">
-				<a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>">
-					<?php
-					$custom_logo_id = get_theme_mod( 'custom_logo' );
-					$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+<body>
+	<header class="template_3_head">
+	<!-- navigation menu -->
 
-					if ( has_custom_logo() ) {
-						echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
-					} else if ( strlen( $logo[0] ) != 0 ) {
-						echo '<img src="' . get_bloginfo( 'template_directory' ) . '/assets/images/Temp3_Logo.png' . '" alt="' . get_bloginfo( 'name' ) . '">';
-					} else {
-						echo '<h1>' . get_bloginfo( 'name' ) . '</h1>';
-					}
+		<nav class="navbar">
+			<div class="navbar-container">
+				<div class="logo">
+					<a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>">
+						<?php
+						$custom_logo_id = get_theme_mod( 'custom_logo' );
+						$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
 
-					?>
-				</a>
-			</div>
+						if ( has_custom_logo() ) {
+							echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+						} else if ( strlen( $logo[0] ) != 0 ) {
+							echo '<img src="' . get_bloginfo( 'template_directory' ) . '/assets/images/Temp3_Logo.png' . '" alt="' . get_bloginfo( 'name' ) . '">';
+						} else {
+							echo '<h1>' . get_bloginfo( 'name' ) . '</h1>';
+						}
 
-			<div class="menu-container">
-				<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-					<svg width="35" height="25" viewBox="0 0 35 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-						<path d="M0 2H35" stroke="white" stroke-width="4"/>
-						<path d="M0 12.04H35" stroke="white" stroke-width="4"/>
-						<path d="M5 22.08H35" stroke="white" stroke-width="4"/>
-					</svg>
-				</button>
+						?>
+					</a>
+				</div>
 
-				<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-					<div class="offcanvas-header">
-						<a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>">
-							<?php
-							$custom_logo_id = get_theme_mod( 'custom_logo' );
-							$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+				<div class="menu-container">
+					<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+						<svg width="35" height="25" viewBox="0 0 35 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0 2H35" stroke="white" stroke-width="4"/>
+							<path d="M0 12.04H35" stroke="white" stroke-width="4"/>
+							<path d="M5 22.08H35" stroke="white" stroke-width="4"/>
+						</svg>
+					</button>
 
-							if ( has_custom_logo() ) {
-								echo '<img class="header_logo" src="' . get_theme_mod( 'footer_image_logo', get_bloginfo( 'template_directory' ) . '/assets/images/Temp3_logo2.png' ) . '" alt="' . get_bloginfo( 'name' ) . '">';
-							} else {
-								echo '<img src="' . get_bloginfo( 'template_directory' ) . '/assets/images/Temp3_logo2.png' . '" alt="' . get_bloginfo( 'name' ) . '">';
-							}
-							?>
-						</a>
+					<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+						<div class="offcanvas-header">
+							<a class="navbar-brand" href="<?php bloginfo( 'url' ); ?>">
+								<?php
+								$custom_logo_id = get_theme_mod( 'custom_logo' );
+								$logo           = wp_get_attachment_image_src( $custom_logo_id, 'full' );
 
-						<button type="button" class="close" data-bs-dismiss="offcanvas" aria-label="Close"><i class="fa-sharp fa-solid fa-xmark"></i></button>
-					</div>
+								if ( has_custom_logo() ) {
+									echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+								} else if ( strlen( $logo[0] ) != 0 ) {
+									echo '<img src="' . get_bloginfo( 'template_directory' ) . '/assets/images/Temp3_Logo.png' . '" alt="' . get_bloginfo( 'name' ) . '">';
+								} else {
+									echo '<h1>' . get_bloginfo( 'name' ) . '</h1>';
+								}
+								?>
+							</a>
 
-					<div class="offcanvas-body">
-						<div class="template_3_header_menu">
+							<button type="button" class="close" data-bs-dismiss="offcanvas" aria-label="Close"><i class="fa-sharp fa-solid fa-xmark"></i></button>
+						</div>
 
-							<?php wp_nav_menu( 'primary' ); ?>
+						<div class="offcanvas-body">
+							<div class="template_3_header_menu">
 
+								<?php wp_nav_menu( 'primary' ); ?>
+
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</nav>
-</header>
+		</nav>
+	</header>
